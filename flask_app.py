083f2b052e5b5b0fc669c708028444ef5c98b3bb
JEskils <143608346+JEskilsAIDEV23@users.noreply.github.com>
2024-05-08 13:47:52 +0200

    flask basic html

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
