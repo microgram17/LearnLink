2d55ac8dc2b4c5bb10625d56e8b816025ac15921
Wille Kolu <wille.kolu10@gmail.com>
2024-05-14 13:18:58 +0200

    renamed routes functions to be more descriptive

--- flask_app.py
+++ flask_app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, flash, redirect, url_for
 app = Flask(__name__)
 
 @app.route("/")
-def home_page():
+def category_page():
     return render_template("category_page.html")
 
 @app.route("/register", methods=["GET", "POST"])
@@ -11,7 +11,7 @@ def register_user():
     return render_template("register_user.html")
 
 @app.route("/category/<int:category_id>")
-def category_page(category_id):
+def category_page_by_id(category_id):
     return render_template("category_page.html", category = category)
 
 @app.route("/material/<int:material_id>")
