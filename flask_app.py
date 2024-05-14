4065e8ab721a5e4258c7e6a60351ffb6cae6c786
Wille Kolu <wille.kolu10@gmail.com>
2024-05-14 13:04:45 +0200

    create some endpoints
    home page(category_page), register user, individual category(category_id), material page(material_id)

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request, flash, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home_page():
+    return render_template("category_page.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register_user():
+    return render_template("register_user.html")
+
+@app.route("/category/<int:category_id>")
+def category_page(category_id):
+    return render_template("category_page.html", category = category)
+
+@app.route("/material/<int:material_id>")
+def material_page(material_id):
+    return render_template("material_page.html", material = material)
