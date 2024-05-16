54ca8574dd7871577e030c38c6ac62a94eebde29
Mattias Lindhe <mattias.lindhe@gmail.com>
2024-05-16 15:24:48 +0200

    Add entry checking for seeding
    added database entry checking before seeding the data to avoid getting
    multipple entries of the same data inside the database

--- seed.py
+++ seed.py
@@ -1,22 +1,29 @@
-from models import db, Role, User, Category, SubCategory, Post, Comments, Tags, FileAttachment, PostRating, CommentRating
+from models import db, Category
 
 
 
 
 
 def seed_data():
+    # Check if there are any existing entries in the database
+    if db.session.query(Category).count() == 0:
+        # If no entries in the database seed data
 
-    #Category seed data
-    categories_data = [
-        {'category_name':'Programming'},
-        {'category_name':'Math'},
-        {'category_name':'Language'},
-        {'category_name':'Chemistry'},
-        {'category_name':'Pysics'},
-    ]
+        #Category seed data
+        categories_data = [
+            {'category_name':'Programming'},
+            {'category_name':'Math'},
+            {'category_name':'Language'},
+            {'category_name':'Chemistry'},
+            {'category_name':'Pysics'},
+        ]
 
-    for category_data in categories_data:
-        category = Category(**category_data)
-        db.session.add(category)
+        for category_data in categories_data:
+            category = Category(**category_data)
+            db.session.add(category)
+            
+        db.session.commit()
+    else:
+        #If entries exists, skip seeding
+        print("Database already contains entries. Skipping seeding.")
 
-    db.session.commit()
