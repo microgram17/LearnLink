b486f1a75f705dcfd7528b04d0d6415b716b7f3e
Amer Al-koofee <amer.mohsin@live.com>
2024-05-22 13:57:01 +0200

    adding many to many "Table" for Roles

--- models.py
+++ models.py
@@ -1,21 +1,25 @@
-from sqlalchemy import Boolean, DateTime, Column, Integer, String, ForeignKey, UniqueConstraint
-from sqlalchemy.ext.mutable import MutableList
+from sqlalchemy import Boolean, DateTime, Column, Integer, String, ForeignKey, Table, UniqueConstraint
 from sqlalchemy.orm import relationship, backref
 from flask_sqlalchemy import SQLAlchemy
-from flask_security import UserMixin, RoleMixin, AsaList
+from flask_security import UserMixin, RoleMixin
+import uuid
 
 db = SQLAlchemy()
 
+roles_users = db.Table('roles_users',
+                       db.Column('user_id', db.Integer, db.ForeignKey(
+                           'users.user_id'), primary_key=True),
+                       db.Column('role_id', db.Integer, db.ForeignKey(
+                           'role.id'), primary_key=True)
+                       )
 
-### DB  MODEL DEFINITION
+
 class Role(db.Model, RoleMixin):
     __tablename__ = 'role'
-    role_id = Column(Integer,primary_key=True)
-    role_name = Column(String(64), unique=True)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(64), unique=True)
     permissions = Column(String())
 
-    # 1 to Many relationship: 1 Role have Many User
-    users = relationship('User', back_populates='roles')
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
@@ -25,26 +29,25 @@ class User(db.Model, UserMixin):
     password = Column(String(255), nullable=False)
     last_login_at = Column(DateTime())
     current_login_at = Column(DateTime())
-    role_id = Column(Integer, ForeignKey('role.role_id'))
+    fs_uniquifier = Column(String(255), unique=True,
+                           nullable=False, default=lambda: str(uuid.uuid4()))
+    active = Column(Boolean, default=True)
 
-    # 1 to Many relationship: 1 Role have Many User
-    roles = relationship('Role', back_populates='users')
-    # 1 to Many relationship: 1 User can make Many Posts
-    created_posts  = relationship('Post', back_populates='user')
-    # 1 to Many relationship: 1 User can make Many Comment
+    roles = relationship('Role', secondary=roles_users,
+                         backref=backref('users', lazy='dynamic'))
+    created_posts = relationship('Post', back_populates='user')
     created_comments = relationship('Comments', back_populates='user')
-    # 1 to Many relationship: 1 User can rate Many posts
     post_ratings = relationship('PostRating', back_populates='user')
-    # 1 to Many relatioship: 1 User can rate Many comments
     comment_ratings = relationship('CommentRating', back_populates='user')
+
 
 class Category(db.Model):
     __tablename__ = 'categories'
     category_id = Column(Integer, primary_key=True)
     category_name = Column(String(255))
 
-    # 1 to Many relationship: 1 Category Have Many SubCategory
     subcategories = relationship('SubCategory', back_populates='category')
+
 
 class SubCategory(db.Model):
     __tablename__ = 'subcategories'
@@ -52,10 +55,9 @@ class SubCategory(db.Model):
     sub_category_name = Column(String(255))
     category_id = Column(Integer, ForeignKey('categories.category_id'))
 
-    # 1 to Many relationship: 1 Category Have Many SubCategory
     category = relationship('Category', back_populates='subcategories')
-    # 1 to Many relationship: 1 SubCategory Have Many Posts
-    related_posts = relationship('Post', back_populates='sub_category') 
+    related_posts = relationship('Post', back_populates='sub_category')
+
 
 class Post(db.Model):
     __tablename__ = 'posts'
@@ -68,16 +70,13 @@ class Post(db.Model):
     updated_at = Column(DateTime())
     visibility = Column(String)
 
-    # 1 to Many relationship: 1 User can make Many Posts
     user = relationship('User', back_populates='created_posts')
-    # 1 to Many relationship: 1 SubCategory Have Many Posts
-    sub_category = relationship('SubCategory', back_populates='related_posts') 
-    # 1 to Many relationship: 1 Post has Many Comments
+    sub_category = relationship('SubCategory', back_populates='related_posts')
     related_comments = relationship('Comments', back_populates='related_post')
-    # 1 to 1 relationship: 1 Post Have 1 Rating
-    post_rating = relationship('PostRating', back_populates='rated_post', uselist=False)
-    # 1 to Many relationship: 1 Post can have many Files attached to it
-    files_attached = relationship('FileAttachment', back_populates='attached_post')
+    post_rating = relationship(
+        'PostRating', back_populates='rated_post', uselist=False)
+    files_attached = relationship(
+        'FileAttachment', back_populates='attached_post')
 
 
 class Comments(db.Model):
@@ -85,25 +84,24 @@ class Comments(db.Model):
     comment_id = Column(Integer, primary_key=True)
     post_id = Column(Integer, ForeignKey('posts.post_id'))
     user_id = Column(Integer, ForeignKey('users.user_id'))
-    # Column for self-referential relationship (comments can be made to comments)
     parent_comment_id = Column(Integer, ForeignKey('comments.comment_id'))
     comment_text = Column(String(255))
     created_at = Column(DateTime())
     updated_at = Column(DateTime())
 
-    # 1 to Many relationship: 1 User can make Many Comment
     user = relationship('User', back_populates='created_comments')
-    # 1 to Many relationship: 1 Post has Many Comments
     related_post = relationship('Post', back_populates='related_comments')
-    # 1 to 1 relationship: 1 Comment have 1 Rating
-    comment_rating = relationship('CommentRating', back_populates='rated_comments', uselist=False)
-    # Self-referential relationship for nested comments
-    parent_comment = relationship('Comments', remote_side=[comment_id], backref='child_comments')
+    comment_rating = relationship(
+        'CommentRating', back_populates='rated_comments', uselist=False)
+    parent_comment = relationship('Comments', remote_side=[
+                                  comment_id], backref='child_comments')
+
 
 class Tags(db.Model):
     __tablename__ = 'tags'
     tag_id = Column(Integer, primary_key=True)
     tag_name = Column(String(255))
+
 
 class FileAttachment(db.Model):
     __tablename__ = 'fileattachments'
@@ -112,7 +110,6 @@ class FileAttachment(db.Model):
     file_name = Column(String(255))
     file_url = Column(String(255))
 
-    # 1 to Many relationship: 1 Post can have many Files attached to it
     attached_post = relationship('Post', back_populates='files_attached')
 
 
@@ -124,13 +121,12 @@ class PostRating(db.Model):
     like = Column(Boolean)
     dislike = Column(Boolean)
 
-    # 1 to 1 relationship: 1 Post have 1 Rating
-    rated_post = relationship('Post', back_populates='post_rating', uselist=False)
-    # 1 to Many relationship: 1 User can rate Many posts
+    rated_post = relationship(
+        'Post', back_populates='post_rating', uselist=False)
     user = relationship('User', back_populates='post_ratings')
 
-    # Ensure each user can rate each post only once
     __table_args__ = (UniqueConstraint('post_id', 'user_id'),)
+
 
 class CommentRating(db.Model):
     __tablename__ = 'commentratings'
@@ -140,10 +136,8 @@ class CommentRating(db.Model):
     dislike = Column(Boolean)
     user_id = Column(Integer, ForeignKey('users.user_id'))
 
-    # 1 to 1 relationship: 1 Comment have 1 Rating
-    rated_comments = relationship('Comments', back_populates='comment_rating', uselist=False)
-    # 1 to Many relatioship: 1 User can rate Many comments
+    rated_comments = relationship(
+        'Comments', back_populates='comment_rating', uselist=False)
     user = relationship('User', back_populates='comment_ratings')
-    
-    # Ensure each user can rate each post only once
+
     __table_args__ = (UniqueConstraint('comment_id', 'user_id'),)
