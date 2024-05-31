d8bce7f8ec924293c3790e208857bddd2465aa56
Mattias Lindhe <mattias.lindhe@gmail.com>
2024-05-31 16:15:00 +0200

    Add config.py file
    Added config.py file to take care of app configurations

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = "sqlite:///learnlink.db"
+    SECRET_KEY = '9d8^7F&4s2@Lp#N6'
+    SECURITY_PASSWORD_SALT = 'super-secret-salt'
+    SECURITY_LOGIN_USER_TEMPLATE = 'login.html'
+    SECURITY_MSG_INVALID_PASSWORD = ("Bad username or password", "error")
+    SECURITY_MSG_PASSWORD_NOT_PROVIDED = ("Bad username or password", "error")
+    SECURITY_MSG_USER_DOES_NOT_EXIST = ("Bad username or password", "error")
+    SESSION_COOKIE_SECURE = True
+    SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SAMESITE = 'Lax'
+
+config = Config()
